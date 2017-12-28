b9242832ff20b7edf1f76b2fb235ff077010e9fd
Egor Zhivagin <egorzhivagin@gmail.com>
2017-12-28 15:20:30 +0300

    add header image

--- models/layout/index.js
+++ models/layout/index.js
@@ -2,8 +2,15 @@ const menu = require('./menu');
 const footer = require('./footer');
 
 module.exports = async function (url) {
+    const staticData = await db.collection('main')
+        .findOne(
+            {
+                name: 'seo'
+            }
+        );
     return {
         menu: await menu(url),
         footer: await footer(url),
+        staticData: staticData.value,
     };
 };
