32e07baf23557ba4e3fe1c6f5d75065ccfa2fd10
Egor Zhivagin <egorzhivagin@gmail.com>
2018-01-10 16:23:18 +0300

    large update

--- models/texts.js
+++ models/texts.js
@@ -1,7 +1,25 @@
-
+const _find = require('lodash/find');
 
 module.exports = async function () {
     let texts = await db.collection('texts').find({}).toArray();
+    let music = await db.collection('music').find({}).toArray();
 
-    return texts;
+    // Оставляем только те треки, у которых в БД есть текст
+    music = music.reduce((filtered, track) => {
+        const text = _find(texts, {name: track.title});
+        if (text) {
+            filtered.push({
+                ...track,
+                text: text.text
+            });
+        }
+        return filtered;
+    }, []);
+
+    // Сортировочка по алфавиту
+    music = music.sort((a, b) => {
+        return b.title < a.title;
+    });
+
+    return music;
 };
