ee3eb4aed8979f0112a41ec0f6e5aad5afaea4c9
Egor Zhivagin <egorzhivagin@gmail.com>
2018-07-16 17:19:33 +0300

    fix NodeCollection to Array

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ import Player from './player';
 import $ from "jquery";
 
 $(document).ready(() => {
-    const players = document.querySelectorAll('.track');
+    const players = [...document.querySelectorAll('.track')];
 
     players.forEach(item => {
         const playerContainer = item.querySelector('.audio');
@@ -11,6 +11,4 @@ $(document).ready(() => {
         const audioPlayer = new Player(playerContainer, playerControlsContainer);
         debugger;
     });
-
-    console.info('yes!', players);
 });
